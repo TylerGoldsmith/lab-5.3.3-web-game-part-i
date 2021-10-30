7e7334dc7fdec49b440bae0225fb5e4c1551488b
Tyler Goldsmith <tyler.r.goldsmith@gmail.com>
2021-10-30 05:30:00 -0400

    Adding code to index.js

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+// Append New Pictures
+// >variables = url left(px) bottom(px) 
+function newImage(url, left, bottom) {
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
+// Add Green Character to page
+newImage('assets/green-character.gif', 100, 100)
+// Add Pine Tree to page
+newImage('assets/pine-tree.png', 450, 200)
+// Add Tree to page
+newImage('assets/tree.png', 350, 100)
+// Add Pillar to page
+newImage('assets/pillar.png', 150, 200)
+// Add Crate to page
+newImage('assets/crate.png', 150, 200)
+// Add Well to page
+newImage('assets/well.png', 500, 425)
+// Add Sword to page and remove on button click
+let sword = newImage('assets/sword.png', 500, 405)
+sword.addEventListener('dblclick', function(){
+    sword.remove()
+})
+//  Add Shield to page
+newImage('assets/sheild.png', 165, 185)
+//  Add Staff to page
+newImage('assets/staff.png', 600, 100)
+// 
+
+// 
+// 
